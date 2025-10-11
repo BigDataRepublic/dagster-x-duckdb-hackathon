88d8cfddbba364a1fe274fabb392e73647efd318
Nick Rankovic <nick.rankovic@dexterenergy.ai>
2025-10-11 12:54:26 +0200

    remove final solution

--- src/dagster_x_duckdb_hackathon/defs/assets/connections.py
+++ src/dagster_x_duckdb_hackathon/defs/assets/connections.py
@@ -29,71 +29,12 @@ def build_nearest_hub_asset(location_asset_key: str, hub_asset_key: str) -> dg.A
     return nearest_hub_asset
 
 
-@dg.asset(
-    deps=[dg.AssetKey("users_train_stations"), dg.AssetKey("destinations_train_stations")],
-    kinds={"duckdb", "silver"},
-    group_name="connections",
-)
-def connections_train_stations(context, duckdb: DuckDBResource) -> None:
-    with duckdb.get_connection() as conn:
-        conn.execute("""
-                     CREATE OR REPLACE TABLE connections_train_stations AS
-                        SELECT DISTINCT
-                            o.users_id,
-                            d.destinations_id,
-                            ST_Distance_Sphere(o.geom, d.geom) as connection_distance,
-                            'train' as transport_mode,
-                            connection_distance / 1000 / 100 as travel_time
-                        FROM
-                            users_train_stations o CROSS JOIN destinations_train_stations d
-                     """)
-
-        df = conn.execute("SELECT * FROM connections_train_stations LIMIT 10").df()
-        context.add_output_metadata(metadata={"preview": dg.MetadataValue.md(df.to_markdown())})
-
-
-@dg.asset(
-    deps=[dg.AssetKey("users_airports"), dg.AssetKey("destinations_airports")],
-    kinds={"duckdb", "silver"},
-    group_name="connections",
-)
-def connections_airports(context, duckdb: DuckDBResource) -> None:
-    with duckdb.get_connection() as conn:
-        conn.execute("""
-                     CREATE OR REPLACE TABLE connections_airports AS
-                        SELECT DISTINCT
-                            o.users_id,
-                            d.destinations_id,
-                            ST_Distance_Sphere(o.geom, d.geom) as connection_distance,
-                            'flight' as transport_mode,
-                            connection_distance / 1000 / 500 as travel_time
-                        FROM
-                            users_airports o CROSS JOIN destinations_airports d
-                     """)
-
-        df = conn.execute("SELECT * FROM connections_airports LIMIT 10").df()
-        context.add_output_metadata(metadata={"preview": dg.MetadataValue.md(df.to_markdown())})
-
-
-@dg.asset(deps=[connections_airports, connections_train_stations], kinds={"duckdb", "silver"}, group_name="connections")
-def connections(context, duckdb: DuckDBResource) -> None:
-    with duckdb.get_connection() as conn:
-        conn.execute("""
-                     CREATE OR REPLACE TABLE connections AS
-                        FROM connections_train_stations
-                        UNION ALL BY NAME
-                        FROM connections_airports
-                     """)
-
-        df = conn.execute("SELECT * FROM connections LIMIT 10").df()
-        context.add_output_metadata(metadata={"preview": dg.MetadataValue.md(df.to_markdown())})
-
-
-@dg.definitions
-def defs():
-    location_asset_keys = ["users", "destinations"]
-    hub_asset_keys = ["train_stations", "airports"]
-
-    nearest_hub_assets = [build_nearest_hub_asset(loc, hub) for loc in location_asset_keys for hub in hub_asset_keys]
-
-    return dg.Definitions(assets=[*nearest_hub_assets, connections_airports, connections_train_stations, connections])
+# TODO: add your final definitions for dynamically-created assets here, as `dg.Definitions`.
+# @dg.definitions
+# def defs():
+#     location_asset_keys = ["users", "destinations"]
+#     hub_asset_keys = [...]
+#
+#     nearest_hub_assets = [...]
+#
+#     return dg.Definitions(assets=[...])
--- src/dagster_x_duckdb_hackathon/defs/assets/destinations.py
+++ src/dagster_x_duckdb_hackathon/defs/assets/destinations.py
@@ -2,14 +2,4 @@ import dagster as dg
 from dagster_duckdb import DuckDBResource
 
 
-@dg.asset(kinds={"duckdb", "bronze"}, group_name="destinations")
-def destinations(duckdb: DuckDBResource) -> None:
-    with duckdb.get_connection() as conn:
-        conn.execute("""
-                     CREATE OR REPLACE TABLE destinations AS
-                        SELECT
-                            row_number() OVER () as id,
-                            * exclude (latitude, longitude),
-                            st_point(longitude::double, latitude::double) as geom
-                        FROM 'data/destinations.csv'
-                     """)
+# TODO
